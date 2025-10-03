d211bf3514966b7f2a0d456efd0065d503213222
sharik10ahmed <sharikahmed757@gmail.com>
2025-10-03 20:25:04 +0530

    Add initial implementation of practice.py with variable definitions and print statements

---
+++ practice.py
@@ -0,0 +1,18 @@
+first_name = "Louis"
+last_name = "Martin"
+
+value_1 = 13
+# 1_value = 15 
+# print(1_value)
+print(value_1)
+
+print(first_name)
+
+first_student_name  ="WIlmot"
+
+FIRST_student_name = "SHarik"
+
+print(FIRST_student_name)
+
+# else = 88
+# print(else)
