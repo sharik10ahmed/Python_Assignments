8fba3f9bbeb59c15d1eccf1ffa74fecd8460b62f
sharik10ahmed <sharikahmed757@gmail.com>
2025-10-04 17:34:31 +0530

    Updated

--- practice.py
+++ practice.py
@@ -6,16 +6,22 @@ value_1 = 13
 # print(1_value)
 print(value_1)
 
-print(first_name)
+print(first_name,last_name)
 
-first_student_name  ="WIlmot"
+first_person_name ="SK"
 
-FIRST_student_name = "SHarik"
+FIRST_person_name = "Sharik"
 
-print(FIRST_student_name)
+print(FIRST_person_name)
 
 if value_1 > 10:
     else_value = 88
 else:
     else_value = 0
 print(else_value)
+
+list1 = [10, 20, 30, 40, 50]
+
+print(" 100 not in list1: ", 100 not in list1)
+
+print(not(value_1 > else_value)) # The answer should be true but it will gives false
