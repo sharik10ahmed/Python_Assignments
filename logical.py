894a1730041b4d438294289ce7c0e3e5b878c5de
sharik10ahmed <sharikahmed757@gmail.com>
2025-10-04 19:12:44 +0530

    new changes added

---
+++ logical.py
@@ -0,0 +1,7 @@
+v = 5
+t = 10
+print(v > t and v < t) # False and True = False
+print(v < t and v < t) # True and True = True
+print(not(v < t and v < t)) # not(True and True) = not(True) = False
+a = 10
+b = 3
