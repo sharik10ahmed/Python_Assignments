9c6808da875293ceda06cd199a0b29cbdf761931
sharik10ahmed <sharikahmed757@gmail.com>
2025-10-07 18:04:53 +0530

    file added

---
+++ pr.py
@@ -0,0 +1,10 @@
+print(8*6)
+print(8/6)
+print(8//6) # Floor Division
+print(8%6)  # Modulo or Modular
+print(8**6) # Exponential
+print(8+6)
+print(8-6)
+print(8*6+6-6/6**6//6%6)
+print(8*(6+6-6)/(6**6))
+print(8*(6+6-6)/(6**(6-3)))
