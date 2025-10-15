48799482cecf1e14a1306afea7a209d2e228cf3a
sharik10ahmed <sharikahmed757@gmail.com>
2025-10-15 17:24:15 +0530

    new dir ADDED

---
+++ calc.py
@@ -0,0 +1,10 @@
+import math
+V1 = float(input("Enter First Value -> " ))
+V2 = float(input("Enter Second value -> "))
+
+print("Sum = ",V1+V2)
+print("Diff = ",V1-V2)
+print("Product = ",V1*V2)
+print("Division = ",V1/V2)
+print("Floor = ",math.trunc(V1//V2))
+print("Exponential = ",V1**V2)
---
+++ revise_indexing.py
@@ -0,0 +1,5 @@
+a1 = "Hello"
+print(a1[::2])
+a2 = ["first",10,"Another",44]
+print(a2[0:2]) # prints ['first', 10]
+print(a2[0::1])
