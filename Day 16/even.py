be4e0886c9d3d9f4029d674357763aa91088ee11
sharik10ahmed <sharikahmed757@gmail.com>
2025-10-24 17:44:09 +0530

    nwely added

---
+++ Day 16/even.py
@@ -0,0 +1,6 @@
+# for e in range(1,11):
+#     i = e * 2
+#     print(i)
+i = int(input("Enter -> "))
+for v1 in range(1,i):
+    i == 
--- Day 16/table.py
+++ Day 16/table.py
@@ -1,6 +1,8 @@
 for v in range(19,191,19):
      print("Table -> ",v)
+print("\n")
     
 for v in range(1,11):
      print("Table -> ",v*19)
+
     
