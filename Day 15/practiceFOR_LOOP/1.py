9eb76fc41ccdca818c3d6d26b1608459c1e95314
sharik10ahmed <sharikahmed757@gmail.com>
2025-10-29 23:30:49 +0530

    changes

---
+++ Day 15/practiceFOR_LOOP/1.py
@@ -0,0 +1,5 @@
+list = [20,50,5,4]
+sum = 0
+for i in list:
+    sum = i+sum
+print(sum)
---
+++ whileloop.py
@@ -0,0 +1,7 @@
+sum = 5
+a = sum*2
+while sum == 5:
+    print("content -> ",a)
+    if a == 10:
+        print("")
+    exit()
