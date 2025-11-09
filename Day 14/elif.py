7a97293435a965a9fa4bc0c92284674e8e9922fa
sharik10ahmed <sharikahmed757@gmail.com>
2025-11-09 18:05:52 +0530

    changes

---
+++ Day 14/elif.py
@@ -0,0 +1,11 @@
+my_number = 99
+my_number = int(input("enter the number"))
+
+if my_number > 0:
+     print("this is Positive number")
+   
+elif my_number < 0: 
+     print("the number is Negative ")
+
+else:
+     print("the number is equal to 0")
