4bc78c84afc63b75479bec95da6e6178193bb38c
sharik10ahmed <sharikahmed757@gmail.com>
2025-11-12 19:46:22 +0530

    updated

---
+++ Day 20/dictionary.py
@@ -0,0 +1,8 @@
+my_dict = {
+                    "name": "Sharik",
+                    "age" : 19,
+                    "department": "CSE",
+                    "data" : "11111111111"
+}
+
+print("get all values: ", my_dict.values())
---
+++ Day 20/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print("get all values: ", my_dict.values())
