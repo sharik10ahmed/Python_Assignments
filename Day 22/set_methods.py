19f9034b6bfaee1cf7f9ab348992a4d9686ce43c
sharik10ahmed <sharikahmed757@gmail.com>
2025-11-14 22:31:56 +0530

    changes

--- Day 22/set_methods.py
+++ Day 22/set_methods.py
@@ -1,4 +1,3 @@
 my_set = {22,34,55,66,77,88,110,200}
-print(my_set)  
-my_set.remove(22)
-print("my new set looks like this : ",my_set)
+my_set.remove(111) # error : because 111 does not exist in set.
+print(my_set)
