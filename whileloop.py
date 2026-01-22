abeb44b385bc93042a3dd9873df60b29bfbcb12b
sharik10ahmed <sharikahmed757@gmail.com>
2026-01-22 11:57:46 +0530

    added

--- whileloop.py
+++ whileloop.py
@@ -1,17 +1,9 @@
 sum = 5
 p = sum*2
-<<<<<<< HEAD
 r=p
-=======
-k=p
->>>>>>> 488e2f1ca61fcb1b30918e9a481c763dbf359927
 while sum == 5:
     print("content -> ",p)
     if p == 10:
         print("Exited")
     break
-<<<<<<< HEAD
 print(r)
-=======
-print(k)
->>>>>>> 488e2f1ca61fcb1b30918e9a481c763dbf359927
