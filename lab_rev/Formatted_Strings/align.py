d7b6a32e5aa5f52e1cf7a314a2e33e9beec3be0a
sharik10ahmed <sharikahmed757@gmail.com>
2026-02-20 23:12:45 +0530

    added

---
+++ lab_rev/Formatted_Strings/align.py
@@ -0,0 +1,10 @@
+value = "Python"
+
+# Left align (<)
+print("Left:   |{:<10}|".format(value))
+
+# Center align (^)
+print("Center: |{:^10}|".format(value))
+
+# Right align (>)
+print("Right:  |{:>10}|".format(value))
