2f8bda62cdcbed2350e430c4e7c2d209d40bbc92
sharik10ahmed <sharikahmed757@gmail.com>
2026-02-21 01:17:27 +0530

    added

--- lab_rev/Formatted_Strings/Number_formatting.py
+++ lab_rev/Formatted_Strings/Number_formatting.py
@@ -1,4 +1,4 @@
 num=1234.5678
 print("Two decimal:{.2f}".format(num))
 value="Sharik"
-# =print("Leftalign:{:<10}".format(value)) align center,right,left
+# print("Leftalign:{:<10}".format(value)) align center,right,left
