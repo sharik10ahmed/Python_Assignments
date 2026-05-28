2251238acf9213b25dc1929c8dc8a97cee9e0cc1
sharik10ahmed <sharikahmed757@gmail.com>
2026-05-28 23:06:05 +0530

    added

---
+++ Lab_FIle/Questions/15.py
@@ -0,0 +1,39 @@
+import sys
+
+def calculate_si(principal,rate=5.0,time=2.0):
+    return (principal*rate*time) / 100
+if len(sys.argv) ==3:
+       print("math detected")
+       num1 = float(sys.argv[1])
+       num2 = float(sys.argv[2])
+       print(f"Addition of {num1} and {num2} = {num1 + num2}")
+       print("-----------------------------------\n")
+while True:
+       print("===Main Menu===")
+       print("1. Perform Multiplication and Addition")
+       print("2. SI with default rate=5 and time=2")
+       print("3. SI with custom p,r,t")
+       print("4. EXIT")
+       choice = input("Enter your choice(1-4): ")
+       if choice == '1':
+          a = float(input("Enter num1-> "))
+          b = float(input("Enter num2-> "))
+          print(f"-> Addition: {a+b}")
+          print(f"-> Multiplication {a*b}")
+       elif choice == '2':
+          p = float(input("Enter principal-> "))
+          interest = calculate_si(p)
+          print(f"SI is {interest}")
+       elif choice == '3':
+          p = float(input("Enter principal-> "))
+          r = float(input("Enter rate-> "))
+          t = float(input("Enter time-> "))
+          interest = calculate_si(p,r,t)
+          print(f"SI for custom values-> {interest}")
+       elif choice == '4':
+          print("Exitng the program")
+          break
+       else:
+          print("Invalid choice !")
+       
+       
--- Lab_FIle/Questions/lamda.py
+++ Lab_FIle/Questions/lamda.py
@@ -1,15 +1,25 @@
-# The input list
-numbers = [2, 4, 6, 8]
+# # The input list
+# numbers = [2, 4, 6, 8]
 
-# 1. Assert statement (The Security Guard)
-# It checks if the list is NOT empty. If it is empty, the program stops with an error.
-assert len(numbers) > 0, "Error: The list is empty!"
+# # 1. Assert statement (The Security Guard)
+# # It checks if the list is NOT empty. If it is empty, the program stops with an error.
+# assert len(numbers) > 0, "Error: The list is empty!"
 
-# 2. Lambda function with map
-# lambda x: x**2 is the tiny function that squares each number
-# map() applies that lambda to every item in the 'numbers' list
-squares = list(map(lambda x: x**2, numbers))
+# # 2. Lambda function with map
+# # lambda x: x**2 is the tiny function that squares each number
+# # map() applies that lambda to every item in the 'numbers' list
+# squares = list(map(lambda x: x**2, numbers))
 
-# Display the result
-print("Original numbers:", numbers)
-print("Squared numbers:", squares)
+# # Display the result
+# print("Original numbers:", numbers)
+# print("Squared numbers:", squares)
+
+def display(list,tuple,dict):
+    print("Inside function")
+    print("List: ",list)
+    print("tuple: ",tuple)
+    print("dict: ",dict)
+fruit_list = ["Apple","Mango"]
+Tuple_ = ("Value","COMP")
+dict = {"Value":20,"ITs":55}
+display(fruit_list,Tuple_,dict)
